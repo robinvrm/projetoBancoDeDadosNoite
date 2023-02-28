9636328b687cd4ff6f07bad1e978be01317b3b09
leotrierveiler <leotrierveiler65@hotmail.com>
2023-02-27 22:16:11 -0300

    Função

--- Leonardo/js/functions.js
+++ Leonardo/js/functions.js
@@ -6,10 +6,14 @@ function pathPosts(file) {
   return `../acao/${file}.php`;
 }
 
-function _loadPage(id, file, data = {}, callback = () => {}) {
+function loaderPage(id) {
+  $(`#${id}`).html('Carregando..');
+}
+
+function _loadPage(id, file, data = {}, callback = () => { }) {
   $(`#${id}`).load(pathLoader(file), data, callback);
 }
 
-function _postPage(file, data = {}, callback = () => {}) {
+function _postPage(file, data = {}, callback = () => { }) {
   $.post(pathPosts(file), data, callback);
 }
